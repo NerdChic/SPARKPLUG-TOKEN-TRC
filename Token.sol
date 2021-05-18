8a2f71b93855b1d1a5b76accf54e4407a2a79277
Dustin Hedrick <dustinhedrick@users.noreply.github.com>
2021-05-18 12:35:17 -0500

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("SPARKPLUGZ", "PLGZ", 14) {
-        _mint(msg.sender, 30000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("SPARKPLUGZ", "PLGZ", 18) {
+        _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
     }
 }
